9d144c2ccb6581e6021348e4a6ef194b55697903
cfj82 <92275816+cfj82@users.noreply.github.com>
2021-11-28 13:31:43 -0800

    updated

--- Guess the Number.py
+++ Guess the Number.py
@@ -14,28 +14,26 @@ root.minsize(width=450, height=250)
 
 
 guess_num = StringVar()
-lives = StringVar()
 hint = StringVar()
 
 def start():
     global ran_num
-    lives = int(0)
     ran_num = random.randint(0, 5)  # random number generated
     hint.set("Enter a Number 0 to 5")
 
 
 def play():
-    global ran_num, lives
-    while lives > 4:
-        guess = int(guess_num.get())
-        if guess == ran_num:
-            hint.set("Good Job! You Win!")
-        elif guess > ran_num:  # guess high
-            hint.set("Guess is too High")
-            lives = lives + 1
-        elif guess < ran_num:  # guess low
-            hint.set("Guess is too Low")
-            lives = lives + 1
+    global ran_num
+    guess = int(guess_num.get())
+    if guess == ran_num:
+        hint.set("Good Job! You Win!")
+    elif guess > ran_num:  # guess high
+        hint.set("Guess is too High")
+        entry.delete(0, END)
+    elif guess < ran_num:  # guess low
+        hint.set("Guess is too Low")
+        entry.delete(0, END)
+
 
 
 def quit():
@@ -46,31 +44,31 @@ def quit():
         messagebox.showinfo("Return", "You Will Now Continue Playing")
 
 
-f0 = Frame(root, pady=3, bg="#000000")  # frame for quit and start button
+f0 = Frame(root, pady=3, bg="#000000")  # frame 1 for start instructions
 f0.pack(expand=True, fill="both")
 # hint label
 title = Label(f0, font=("verdana", 12, "italic"), anchor="center",
                     bg="#5DADE2", text="---Press 'Start Game' to Play---", )
 title.pack(expand=True, fill="both", pady=3, padx=8)
 
-f1 = Frame(root, pady=3, bg="#000000")  # frame for quit and start button
+f1 = Frame(root, pady=3, bg="#000000")  # frame 2 for lives
 f1.pack(expand=True, fill="both")
-
+"""
 lives_lbl = Label(f1, font=("verdana", 18, "italic"), anchor="center",
                     bg="#5DADE2", textvariable=lives)
 lives_lbl.pack(expand=True, fill="both", pady=3, padx=6)
-
+"""
 f2 = Frame(root, pady=3, bg="#000000")  # frame for number of guesses
 f2.pack(expand=True, fill="both")
 
 # entry field
-entry = Entry(f2, font=("verdana", 15),
-                    bg="#D5D8DC", text="Submit",
+entry = Entry(f2, font=("verdana", 15), relief=SUNKEN, border=10,
+                bg="#D5D8DC", text="Submit", width=15,
               textvariable=guess_num)
-entry.pack(side=LEFT, expand=True, fill="both", pady=3, padx=6)
+entry.pack(side=LEFT, expand=True, fill="y",)
 
 # submit button
-submit_btn = Button(f2, font=("verdana", 15), anchor="center",
+submit_btn = Button(f2, font=("verdana", 15), anchor="center", relief=GROOVE, border=10,
                     bg="#5DADE2", text="Submit", command=play)
 submit_btn.pack(side=LEFT, expand=True, fill="both", pady=3, padx=8)
 
@@ -78,7 +76,7 @@ f3 = Frame(root, pady=3, bg="#000000")  # frame for hint
 f3.pack(expand=True, fill="both")
 
 # hint label
-hint_lbl = Label(f3, font=("verdana", 18), anchor="center",
+hint_lbl = Label(f3, font=("verdana", 18), anchor="center", relief=SUNKEN, border=10,
                     bg="#5DADE2",
                  textvariable=hint)
 hint_lbl.pack(expand=True, fill="both", pady=3, padx=8)
@@ -87,12 +85,12 @@ f4 = Frame(root, pady=3, bg="#000000")  # frame for number guess
 f4.pack(expand=True, fill="both")
 
 # button to quit game
-quit = Button(f4, font=("verdana", 15), anchor="center",
+quit = Button(f4, font=("verdana", 15), anchor="center", relief=GROOVE, border=10,
                     bg="#D2B4DE", text="Quit", command=quit)
 quit.pack(side=LEFT, expand=True, fill="both", pady=3, padx=6)
 
 # button to start game
-start = Button(f4, font=("verdana", 15), anchor="center",
+start = Button(f4, font=("verdana", 15), anchor="center", relief=GROOVE, border=10,
                     bg="#D2B4DE", text="Start Game", command=start)
 start.pack(side=LEFT, expand=True, fill="both", pady=3, padx=6)
 

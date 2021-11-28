4e0db05c0d0b738877c73b353fc5419fddb8a1c1
cfj82 <92275816+cfj82@users.noreply.github.com>
2021-11-28 12:39:59 -0800

    updated

--- Guess the Number.py
+++ Guess the Number.py
@@ -1,123 +1,107 @@
+# Guess the Number with Entry Field
+
 #  Guess the Number
 #  Program generate random number and user will guess. After each guess a hint will be given
 
 import random
-import tkinter as tk
+from tkinter import *
 from tkinter import messagebox
-import threading
 
 
-root = tk.Tk()
+root = Tk()
 root.title("Number Guess Game")
 root.minsize(width=450, height=250)
 
-f1 = tk.Frame(root, pady=3, bg="#000000")  # frame for quit and start button
-f1.pack(expand=True, fill="both")
 
-f2 = tk.Frame(root, pady=3, bg="#000000")  # frame for number of guesses
-f2.pack(expand=True, fill="both")
+guess_num = StringVar()
+lives = StringVar()
+hint = StringVar()
 
-f3 = tk.Frame(root, pady=3, bg="#000000")  # frame for hint
-f3.pack(expand=True, fill="both")
-
-f4 = tk.Frame(root, pady=3, bg="#000000")  # frame for number guess
-f4.pack(expand=True, fill="both")
-
-f5 = tk.Frame(root, pady=3, bg="#000000")
-f5.pack(expand=True, fill="both")
-
-guess = int
-
-def quit():
-    msg = tk.messagebox.askyesno("Quit Game", "Would You Like To Quit Game?")
-    if msg == True:
-        root.destroy()
-    else:
-        tk.messagebox.showinfo("Return", "You Will Now Continue Playing")
-
-def b1():
-    global guess
-    guess = int(1)
-    num_guess_output.set("1")
-
-def b2():
-    global guess
-    guess = int(2)
-    num_guess_output.set("2")
-
-def b3():
-    global guess
-    guess = int(3)
-    num_guess_output.set("3")
-
-def b4():
-    global guess
-    guess = int(4)
-    num_guess_output.set("4")
-
-def b5():
-    global guess
-    guess = int(5)
-    num_guess_output.set("5")
+def start():
+    global ran_num
+    lives = int(0)
+    ran_num = random.randint(0, 5)  # random number generated
+    hint.set("Enter a Number 0 to 5")
 
 
 def play():
-    global guess
-    ran_num = random.randint(0, 5)  # random number generated
-    num_guess_output.set('Please guess a number from 0 to 5')  # user input guess
+    global ran_num, lives
+    while lives > 4:
+        guess = int(guess_num.get())
+        if guess == ran_num:
+            hint.set("Good Job! You Win!")
+        elif guess > ran_num:  # guess high
+            hint.set("Guess is too High")
+            lives = lives + 1
+        elif guess < ran_num:  # guess low
+            hint.set("Guess is too Low")
+            lives = lives + 1
 
-    if guess == ran_num:  # guess correct
-        hint_output.set("Good Guess! You Win!")
+
+def quit():
+    msg = messagebox.askyesno("Quit Game", "Would You Like To Quit Game?")
+    if msg == True:
         root.destroy()
+    else:
+        messagebox.showinfo("Return", "You Will Now Continue Playing")
 
-    elif guess > ran_num:  # guess high
-        hint_output.set("Guess is too High")
 
-    elif guess < ran_num:  # guess low
-        hint_output.set("Guess is too Low")
+f0 = Frame(root, pady=3, bg="#000000")  # frame for quit and start button
+f0.pack(expand=True, fill="both")
+# hint label
+title = Label(f0, font=("verdana", 12, "italic"), anchor="center",
+                    bg="#5DADE2", text="---Press 'Start Game' to Play---", )
+title.pack(expand=True, fill="both", pady=3, padx=8)
 
+f1 = Frame(root, pady=3, bg="#000000")  # frame for quit and start button
+f1.pack(expand=True, fill="both")
+
+lives_lbl = Label(f1, font=("verdana", 18, "italic"), anchor="center",
+                    bg="#5DADE2", textvariable=lives)
+lives_lbl.pack(expand=True, fill="both", pady=3, padx=6)
+
+f2 = Frame(root, pady=3, bg="#000000")  # frame for number of guesses
+f2.pack(expand=True, fill="both")
+
+# entry field
+entry = Entry(f2, font=("verdana", 15),
+                    bg="#D5D8DC", text="Submit",
+              textvariable=guess_num)
+entry.pack(side=LEFT, expand=True, fill="both", pady=3, padx=6)
+
+# submit button
+submit_btn = Button(f2, font=("verdana", 15), anchor="center",
+                    bg="#5DADE2", text="Submit", command=play)
+submit_btn.pack(side=LEFT, expand=True, fill="both", pady=3, padx=8)
+
+f3 = Frame(root, pady=3, bg="#000000")  # frame for hint
+f3.pack(expand=True, fill="both")
+
+# hint label
+hint_lbl = Label(f3, font=("verdana", 18), anchor="center",
+                    bg="#5DADE2",
+                 textvariable=hint)
+hint_lbl.pack(expand=True, fill="both", pady=3, padx=8)
+
+f4 = Frame(root, pady=3, bg="#000000")  # frame for number guess
+f4.pack(expand=True, fill="both")
 
 # button to quit game
-f1_quit = tk.Button(f1, font=("verdana", 15), anchor="center",
-                    bg="#5DADE2", text="Quit", command=quit)
-f1_quit.pack(side=tk.LEFT, expand=True, fill="both")
+quit = Button(f4, font=("verdana", 15), anchor="center",
+                    bg="#D2B4DE", text="Quit", command=quit)
+quit.pack(side=LEFT, expand=True, fill="both", pady=3, padx=6)
+
 # button to start game
-f1_start = tk.Button(f1, font=("verdana", 15), anchor="center",
-                    bg="#5DADE2", text="Start Here", command=play)
-f1_start.pack(side=tk.LEFT, expand=True, fill="both")
+start = Button(f4, font=("verdana", 15), anchor="center",
+                    bg="#D2B4DE", text="Start Game", command=start)
+start.pack(side=LEFT, expand=True, fill="both", pady=3, padx=6)
+
 
 # number of guesses output
-num_guess_output = tk.StringVar()
-num_guess_lbl = tk.Label(f2, font=("verdana", 15), anchor="center",
+num_guess_output = StringVar()
+num_guess_lbl = Label(f2, font=("verdana", 15), anchor="center",
                     bg="#5DADE2", textvariable=num_guess_output)
 num_guess_lbl.pack(expand=True, fill="both")
-
-# hint output
-hint_output = tk.StringVar()
-hint_output_lbl = tk.Label(f3, font=("verdana", 15), anchor="center",
-                    bg="#5DADE2", textvariable=hint_output)
-hint_output_lbl.pack(expand=True, fill="both")
-
-# buttons
-btn1 = tk.Button(f4, font=("verdana", 15), anchor="center",
-                    bg="#5DADE2", text="1", command=b1)
-btn1.pack(side=tk.LEFT, expand=True, fill="both")
-
-btn2 = tk.Button(f4, font=("verdana", 15), anchor="center",
-                    bg="#5DADE2", text="2", command=b2)
-btn2.pack(side=tk.LEFT, expand=True, fill="both")
-
-btn3 = tk.Button(f4, font=("verdana", 15), anchor="center",
-                    bg="#5DADE2", text="3", command=b3)
-btn3.pack(side=tk.LEFT, expand=True, fill="both")
-
-btn4 = tk.Button(f4, font=("verdana", 15), anchor="center",
-                    bg="#5DADE2", text="4", command=b4)
-btn4.pack(side=tk.LEFT, expand=True, fill="both")
-
-btn5 = tk.Button(f4, font=("verdana", 15), anchor="center",
-                    bg="#5DADE2", text="5", command=b5)
-btn5.pack(side=tk.LEFT, expand=True, fill="both")
 
 
 
